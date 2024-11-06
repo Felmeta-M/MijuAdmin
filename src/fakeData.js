073d2eed588d6c3b230775fbe6638c9c403d5b5d
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-11-06 20:44:34 +0300

    commit

---
+++ src/fakeData.js
@@ -0,0 +1,233 @@
+import profile from "../src/assets/profile.jpg";
+
+export const userData = [
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "Mar",
+      "Active User": 2000,
+    },
+    {
+      name: "Apr",
+      "Active User": 2780,
+    },
+    {
+      name: "May",
+      "Active User": 1890,
+    },
+    {
+      name: "Jun",
+      "Active User": 2390,
+    },
+    {
+      name: "Jul",
+      "Active User": 3490,
+    },
+    {
+      name: "Aug",
+      "Active User": 1865,
+    },
+    {
+      name: "Sep",
+      "Active User": 4490,
+    },
+    {
+      name: "Oct",
+      "Active User": 2490,
+    },
+    {
+      name: "Nov",
+      "Active User": 5490,
+    },
+    {
+      name: "Dec",
+      "Active User": 1490,
+    },
+  ];
+
+  export const productData = [
+    {
+      name: "Jan",
+      "Sales": 4000,
+    },
+    {
+      name: "Feb",
+      "Sales": 3000,
+    },
+    {
+      name: "Mar",
+      "Sales": 5000,
+    },
+  ];
+
+  export const userRows = [
+    {
+      id: 1,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 2,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 3,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 4,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 5,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 6,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 7,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 8,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 9,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+    {
+      id: 10,
+      username: "Redi Tason",
+      avatar: profile,
+      email: "redi@gmail.com",
+      status: "active",
+      transaction: "$350.00",
+    },
+  ];
+
+  export const productRows = [
+    {
+      id: 1,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 2,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 3,
+     name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 4,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 5,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 6,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 7,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 8,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 9,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+    {
+      id: 10,
+      name: "LV Tshirt",
+      img: "https://eu.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-graphic-lv-print-t-shirt-ready-to-wear--FQTS22IIX810_PM2_Front%20view.png?wid=2400&hei=2400",
+      stock: 50,
+      status: "active",
+      price: "$350.00",
+    },
+  ];
